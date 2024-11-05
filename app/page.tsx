24f37b44d80123889da4abb459f4be1ae03b595c
moehami <m.aljaaly@yahoo.co.uk>
2024-11-05 17:41:49 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client"
-import { useState } from 'react';
+// pages/index.tsx
+import { useState, FormEvent, ChangeEvent } from 'react';
 import { useRouter } from 'next/router';
 import { Alert, Button } from "flowbite-react";
 
 export default function Home() {
   const router = useRouter();
-  const [selectedState, setSelectedState] = useState('');
+  const [selectedState, setSelectedState] = useState<string>('');
 
-const states = [
+  const states: string[] = [
     'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
     'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois',
     'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
@@ -18,7 +19,7 @@ const states = [
     'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
   ];
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (selectedState) {
       router.push(`/places?state=${selectedState}`);
@@ -26,21 +27,20 @@ const states = [
   };
 
   return (
+    <div className="flex items-center justify-center h-screen">
+      <Alert color="warning" rounded>
+        <span className="font-medium">Info alert!</span> Change a few things up
+        and try submitting again.
+      </Alert>
 
- <div className="flex items-center justify-center h-screen">
-   <Alert color="warning" rounded>
-      <span className="font-medium">Info alert!</span> Change a few things up
-      and try submitting again. MOe
-    </Alert>
-
-   <Button>Click me</Button>
-        
+      <Button>Click me</Button>
+      
       <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md w-80">
         <h1 className="text-2xl font-bold mb-4">Select a State</h1>
         <select
           required
           value={selectedState}
-          onChange={(e) => setSelectedState(e.target.value)}
+          onChange={(e: ChangeEvent<HTMLSelectElement>) => setSelectedState(e.target.value)}
           className="w-full mb-4 p-2 border border-gray-300 rounded-lg"
         >
           <option value="">Choose a state</option>
