41017261e62bcf89c3c6718c1035884452b769ac
moehami <m.aljaaly@yahoo.co.uk>
2024-11-05 22:24:35 +0200

    Update BinStoresList.tsx

--- app/places/BinStoresList.tsx
+++ app/places/BinStoresList.tsx
@@ -12,16 +12,16 @@ export function BinStoresList({ binStores }: BinStoresListProps) {
   return (
     <div>
       {binStores.length > 0 ? (
-        binStores.map((store) => (
+        binStores.map((data) => (
           <div key={store.fsq_id} className="mb-4 p-4 border border-gray-200 roundedo-lg shadow">
-            <h2 className="text-xl font-bold">{store.name}</h2>
-            <p><strong>Address:</strong> {store.location?.formatted_address || "Not available"}</p>
-            <p><strong>Phone:</strong> {store.contact?.phone || "Not available"}</p>
-            {store.website ? (
+            <h2 className="text-xl font-bold">{data.name}</h2>
+            <p><strong>Address:</strong> {data.location?.formatted_address || "Not available"}</p>
+            <p><strong>Phone:</strong> {data.contact?.phone || "Not available"}</p>
+            {data.website ? (
               <p>
                 <strong>Website:</strong>{' '}
-                <a href={store.website} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
-                  {store.website}
+                <a href={data.website} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+                  {data.website}
                 </a>
               </p>
             ) : (
