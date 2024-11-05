643247f624779fe0773918d1038fec8cbc201bd0
moehami <m.aljaaly@yahoo.co.uk>
2024-11-05 20:04:59 +0200

    Update page.tsx

--- app/places/page.tsx
+++ app/places/page.tsx
@@ -60,9 +60,10 @@ const PlacesPage: NextPage<PageProps> = async ({ searchParams }) => {
 
   return (
      <div className="bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <h1 className="text-3xl font-bold tracking-tight text-gray-900">Bin Stores in {state}</h1>
       <BinStoresList binStores={binStores} />
-    </div>
+    </div></div>
   );
 };
 
